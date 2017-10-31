7f4d46e0e090bc7402a8170e77d39f81076809de
julio cornelio <empjulio@hotmail.com>
2017-10-31 10:59:37 -0500

    all steps are set, score adding up

--- src/Temp.js
+++ src/Temp.js
@@ -18,31 +18,35 @@ class App extends React.Component {
         header: 'What is JavaScript?', 
         subhead: 'JavaScript simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
         quest: ['class based', 'Object oriented and dynamic', 'static', 'complied program'],
-        answer: {ans: 'Object oriented and dynamic', points: '25'}
+        answer: {text: 'Object oriented and dynamic', points: 25}
       },
       {
         header: 'What is React?', 
         subhead: 'React simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-        quest: ['I agree', 'For sure', 'libraray', 'framework']
+        quest: ['build', 'view layer', 'state management', 'framework'],
+        answer: {text: 'view layer', points: 25}
       },
       {
         header: 'What is Redux?', 
         subhead: 'Redux simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-        quest: ['React library', 'garden tool', 'state management library']
+        quest: ['React library', 'garden tool', 'state management library'],
+        answer: {text: 'state management library', points: 25}
       },
       {
         header: 'What is CSS?', 
         subhead: 'CSS simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-        quest: ['library', 'framework', 'API', 'styling standard']
+        quest: ['library', 'framework', 'API', 'language for styling'],
+        answer: {text: 'language for styling', points: 25}
       }
-    ]
+    ],
+    score: 0
   };
 
   handleNext = () => {
     const {stepIndex} = this.state;
     this.setState({
       stepIndex: stepIndex + 1,
-      finished: stepIndex >= 2,
+      finished: stepIndex >= 3,
     });
   };
 
@@ -53,8 +57,14 @@ class App extends React.Component {
     }
   };
 
-  _respond = (answer) => {
-    console.log(answer)
+  _respond = (response, idx) => {
+    console.log(response, idx);
+    console.log(this.state.questions[idx].answer.text);
+
+    if(this.state.questions[idx].answer.text === response){
+      this.setState({score: this.state.score += this.state.questions[idx].answer.points})
+    }
+    console.log('SCORE: --------------------------> ', this.state.score);
   }
 
   _renderFirst = () => {
@@ -62,8 +72,8 @@ class App extends React.Component {
       <div>
         <h2>{this.state.questions[0].header}</h2>
         <p>{this.state.questions[0].subhead}</p>
-        {this.state.questions[0].quest.map( (item, i) => {
-          return <button onClick={this._respond.bind(this, this.state.questions[0].quest)} key={i}>{item}</button>
+        {this.state.questions[0].quest.map( (item, idx) => {
+          return <button onClick={this._respond.bind(this, item, 0)} key={idx}>{item}</button>
         })}
       </div>
     )
@@ -75,7 +85,7 @@ class App extends React.Component {
         <h2>{this.state.questions[1].header}</h2>
         <p>{this.state.questions[1].subhead}</p>
         {this.state.questions[1].quest.map( (item, i) => {
-          return <button key={i}>{item}</button>
+          return <button onClick={this._respond.bind(this, item, 1)} key={i}>{item}</button>
         })}
       </div>
     )
@@ -87,7 +97,7 @@ class App extends React.Component {
         <h2>{this.state.questions[2].header}</h2>
         <p>{this.state.questions[2].subhead}</p>
         {this.state.questions[2].quest.map( (item, i) => {
-          return <button key={i}>{item}</button>
+          return <button onClick={this._respond.bind(this, item, 2)} key={i}>{item}</button>
         })}
       </div>
     )
@@ -99,7 +109,7 @@ class App extends React.Component {
         <h2>{this.state.questions[3].header}</h2>
         <p>{this.state.questions[3].subhead}</p>
         {this.state.questions[3].quest.map( (item, i) => {
-          return <button key={i}>{item}</button>
+          return <button onClick={this._respond.bind(this, item, 3)} key={i}>{item}</button>
         })}
       </div>
     )
@@ -113,6 +123,8 @@ class App extends React.Component {
         return this._renderSecond();
       case 2:
         return this._renderThird();
+      case 3:
+        return this._renderFourth();
       default:
         return this._renderFourth();
     }
@@ -138,17 +150,19 @@ class App extends React.Component {
           </Stepper>
           <div style={contentStyle}>
             {finished ? (
-              <p>
+              <div>
+                <h1>Your Total Score: {this.state.score}</h1>
+                { this.state.score > 50 ? <h1>CONGRATULATIONS</h1> : <p>please try again</p> }
                 <a
                   href="#"
                   onClick={(event) => {
                     event.preventDefault();
-                    this.setState({stepIndex: 0, finished: false});
+                    this.setState({stepIndex: 0, finished: false, score: 0});
                   }}
                 >
-                  Click here
-                </a> to reset the example.
-              </p>
+                  Reset test
+                </a>
+              </div>
             ) : (
               <div>
                 <div>{this.getStepContent(stepIndex)}</div>
@@ -160,7 +174,7 @@ class App extends React.Component {
                     style={{marginRight: 12}}
                   />
                   <RaisedButton
-                    label={stepIndex === 2 ? 'Finish' : 'Next'}
+                    label={stepIndex === 3 ? 'Finish' : 'Next'}
                     primary={true}
                     onClick={this.handleNext}
                   />
