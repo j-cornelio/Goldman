ebfef1b6d19b4e31bb595757b2d85d5407d9896c
julio cornelio <empjulio@hotmail.com>
2017-11-09 10:22:59 -0600

    refactored

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 import React            from 'react';
 import RaisedButton     from 'material-ui/RaisedButton';
 import FlatButton       from 'material-ui/FlatButton';
+import questions        from './data/questions';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      finished: false,
-      stepIndex: 0,
+      finished    : false,
+      stepIndex   : 0,
 
-      questions: [
-        {
-          header: 'What is JavaScript?', 
-          subhead: 'JavaScript simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-          quest: ['class based', 'Object oriented and dynamic', 'static', 'complied program'],
-          answer: {text: 'Object oriented and dynamic', points: 25}
-        },
-        {
-          header: 'What is React?', 
-          subhead: 'React simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-          quest: ['build', 'view layer', 'state management', 'framework'],
-          answer: {text: 'view layer', points: 25}
-        },
-        {
-          header: 'What is Redux?', 
-          subhead: 'Redux simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-          quest: ['React library', 'garden tool', 'state management library'],
-          answer: {text: 'state management library', points: 25}
-        },
-        {
-          header: 'What is CSS?', 
-          subhead: 'CSS simply is dummy Lorem Ipsum simply is dummy Lorem Ipsum simply is dummy', 
-          quest: ['library', 'framework', 'API', 'language for styling'],
-          answer: {text: 'language for styling', points: 25}
-        }
-      ],
+      questions,
       score: 0
     };
   }
